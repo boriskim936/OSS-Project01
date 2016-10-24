cee2f84b969e788038dfbed16f146f50605f2d1c
JuHyun <wngus55555@naver.com>
2016-10-24 17:13:06 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 void main()
 {
   printf("listen listen i can't listen=듣자 듣자 하니 못들어주겠만");
-  printf("2012244048 김경보");//2학 
+  printf("2012244048 김경보");//2학
+  PRINTF("2015244043 주현");//2학 현이라 불러주세요
   return;
 }
