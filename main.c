6b5efe966d692fa2b681d750b6d4cfb0f4e49b85
suhee96 <rkdtngml96@naver.com>
2016-10-25 17:04:52 +0900

    Update main.c

--- main.c
+++ main.c
@@ -5,5 +5,6 @@ void main()
   printf("listen listen i can't listen=듣자 듣자 하니 못들어주겠만");
   printf("2012244048 김경보");//2학
   PRINTF("2015244043 주현");//2학 현이라 불러주세요
+  printf("2015244027 강수희");//2학년
   return;
 }
